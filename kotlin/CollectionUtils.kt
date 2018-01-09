bd563d21b2276364a1fdc95d0bb4238a3581b0c9
Suppoze <tegyukfel@gmail.com>
2018-01-09 22:59:58 +0100

    Added sleep sort to CollectionUtils

--- kotlin/CollectionUtils.kt
+++ kotlin/CollectionUtils.kt
@@ -1,5 +1,9 @@
 import com.google.common.collect.Ordering
-import java.util.Collections
+import kotlinx.coroutines.experimental.channels.Channel
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
+import java.util.concurrent.TimeUnit
 
 object CollectionUtils {
     fun <T : Comparable<Nothing>> sort(unsortedList: List<T>): List<T> {
@@ -7,10 +11,26 @@ object CollectionUtils {
         var ordered = Ordering.natural<T>().isOrdered(everyday)
 
         while (!ordered) {
-            Collections.shuffle(everyday) // Everyday I'm shufflin'
+            everyday.shuffle() // Everyday I'm shufflin'
             ordered = Ordering.natural<T>().isOrdered(everyday)
         }
 
         return everyday
+    }
+
+    fun sleepSort(unsortedList: List<Long>): List<Long> {
+        val newList = mutableListOf<Long>()
+        val channel = Channel<Long>()
+
+        unsortedList.forEach {
+            launch {
+                delay(it, TimeUnit.SECONDS)
+                channel.send(it)
+            }
+        }
+
+        (1..unsortedList.size).forEach { runBlocking { newList.add(channel.receive()) } }
+
+        return newList
     }
 }
